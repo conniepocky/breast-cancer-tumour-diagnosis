5ed88ae494fea252ca300b2d8875d3a0ea4d67ce
Connie <c.amer@icloud.com>
2024-07-25 16:34:21 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.decomposition import PCA
+
+
+data = pd.read_csv("data/data.csv")
+data.drop("id", axis=1, inplace=True)
+
+#min max scaling/normalization
+
+numerical_cols = ["radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean", "compactness_mean", "concavity_mean", "concave_points_mean", "symmetry_mean", "fractal_dimension_mean", "radius_se", "texture_se", "perimeter_se", "area_se", "smoothness_se", "compactness_se", "concavity_se", "concave_points_se", "symmetry_se", "fractal_dimension_se", "radius_worst", "texture_worst", "perimeter_worst", "area_worst", "smoothness_worst", "compactness_worst", "concavity_worst", "concave_points_worst", "symmetry_worst", "fractal_dimension_worst"]
+
+data[numerical_cols] = (data[numerical_cols] - data[numerical_cols].min()) / (data[numerical_cols].max() - data[numerical_cols].min()) # min-max scaling, all values will be between 0 and 1
+
+# diagnosis label encoding, 0 or 1
+
+label_encoder = LabelEncoder()
+
+data["diagnosis"] = label_encoder.fit_transform(data["diagnosis"])
+
+print(data.head())
+
+# train test split
+
+X = data.drop("diagnosis", axis=1)
+y = data["diagnosis"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) #20% test data
+
+# model
+
+knn = KNeighborsClassifier(n_neighbors=3)
+
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+
+print("Accuracy: ", accuracy_score(y_test, y_pred))
+print("Precision: ", precision_score(y_test, y_pred))
+print("Recall: ", recall_score(y_test, y_pred)) 
